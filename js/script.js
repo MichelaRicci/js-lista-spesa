9e6f0abf3decfb65e150f576c6d8aa2608c43228
MichelaRicci <michela.ricci.1991@gmail.com>
2022-12-14 16:27:07 +0100

    elementi inseriti console.log

---
+++ js/script.js
@@ -0,0 +1,21 @@
+console.log('JS OK');
+
+// Data una lista della spesa, stampare in pagina gli elementi della lista individualmente 
+// con un ciclo while.
+
+
+// Creo array lista spesa
+
+const shoppingList = ['pane', 'olio evo', 'parmigiano', 'zucca', 'rosmarino'];
+
+console.log(shoppingList);
+
+
+let i = 0;
+
+
+while (i < shoppingList.length) {
+    console.log(shoppingList[i]);
+    i++;
+
+}
